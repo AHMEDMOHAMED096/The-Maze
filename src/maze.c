f734d4ac596e90fdfe882358f833de91ad1c3baa
ahmed mohamed <ahmed.mohamed91196@gmail.com>
2024-09-13 23:09:50 +0300

    created helpful functions for more readability

--- src/maze.c
+++ src/maze.c
@@ -4,58 +4,6 @@ Player Attributes = {1.5, 1.5, 0, 0.05, 0.05};
 int map[MAPHEIGHT][MAPWIDTH] = {0};
 
 /**
- * castRays - Casts rays to render the 3D view of the game world.
- * @renderer: The SDL_Renderer to use for rendering the game.
- * @wallTexture: The SDL_Texture to use for rendering the walls.
- * @floorTexture: The SDL_Texture to use for rendering the floor.
- */
-
-void castRays(SDL_Renderer *renderer, SDL_Texture *wallTexture,
-			  SDL_Texture *floorTexture)
-{
-	float rayAngle, rayX, rayY, rayDirX, rayDirY, distance;
-	bool hit, isNorthSouthWall;
-	int mapX, mapY, lineHeight, x;
-
-	for (x = 0; x < WINDOW_WIDTH; x++)
-	{
-		rayAngle = Attributes.playerDir - 0.5f + (float)x / WINDOW_WIDTH;
-		rayX = Attributes.x;
-		rayY = Attributes.y;
-		rayDirX = cos(rayAngle);
-		rayDirY = sin(rayAngle);
-		hit = false;
-		isNorthSouthWall = false;
-		while (!hit)
-		{
-			rayX += rayDirX * 0.01f;
-			rayY += rayDirY * 0.01f;
-			mapX = (int)rayX;
-			mapY = (int)rayY;
-
-			if (mapX >= 0 && mapX < MAPWIDTH && mapY >= 0 && mapY < MAPHEIGHT)
-			{
-				if (map[mapX][mapY] == 1)
-				{
-					hit = true;
-					if (fabs(rayDirX) > fabs(rayDirY))
-						isNorthSouthWall = false;
-					else
-						isNorthSouthWall = true;
-					distance = sqrt((rayX - Attributes.x) * (rayX - Attributes.x) +
-									(rayY - Attributes.y) * (rayY - Attributes.y));
-					lineHeight = (int)(WINDOW_HEIGHT / distance);
-					drawMap(renderer, wallTexture, floorTexture,
-							x, lineHeight, isNorthSouthWall);
-				}
-			}
-			else
-				hit = true;
-		}
-	}
-}
-
-/**
  * destroySDL - Cleans up and destroys SDL resources.
  * @wallTexture: The SDL_Texture to be destroyed.
  * @floorTexture: The SDL_Texture to be destroyed.
@@ -79,57 +27,57 @@ void destroySDL(SDL_Texture *wallTexture, SDL_Texture *floorTexture,
 }
 
 /**
- * main - Entry point to the game.
- * @argc: Command line arguments number.
- * @args: Command line arguments.
- * Return: 0 on success.
+ * initializeSDL - Initializes the SDL library
+ *  and creates a window and renderer.
+ * @Window: A double pointer to the SDL_Window to be created.
+ * @Renderer: A double pointer to the SDL_Renderer to be created.
+ * Return: True on success.
  */
 
-int main(int argc, char *args[])
+bool initializeSDL(SDL_Window **Window, SDL_Renderer **Renderer)
 {
-
-	if (argc != 2)
-	{
-		fprintf(stderr, "Usage: %s <map file>\n", args[0]);
-		return (EXIT_FAILURE);
-	}
-	SDL_Window *Window = NULL;
-	SDL_Renderer *Renderer = NULL;
-
 	if (SDL_Init(SDL_INIT_VIDEO) < 0)
 	{
 		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-		return (EXIT_FAILURE);
+		return (false);
 	}
-	else
-	{
-		Window = SDL_CreateWindow("The Maze", SDL_WINDOWPOS_UNDEFINED,
-								  SDL_WINDOWPOS_UNDEFINED,
-								  WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
-		if (Window == NULL)
-		{
-			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
-			return (EXIT_FAILURE);
-		}
-		else
-		{
-			Renderer = SDL_CreateRenderer(Window, -1, SDL_RENDERER_ACCELERATED);
-			if (Renderer == NULL)
-			{
-				printf("Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
-				SDL_DestroyWindow(Window);
-				SDL_Quit();
-				return (EXIT_FAILURE);
-			}
-		}
-	}
-	SDL_Texture *wallTexture = NULL, *floorTexture = NULL;
-	bool quit = false, showMap = false, moveForward = false, moveBackward = false;
-	bool moveRight = false, rotateLeft = false,
-		 rotateRight = false, moveLeft = false;
 
-	wallTexture = loadTexture("wallTexture.jpg", Renderer);
-	floorTexture = loadTexture("floorTexture.jpg", Renderer);
+	*Window = SDL_CreateWindow("The Maze", SDL_WINDOWPOS_UNDEFINED,
+							   SDL_WINDOWPOS_UNDEFINED,
+							   WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
+	if (*Window == NULL)
+	{
+		printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+		return (false);
+	}
+
+	*Renderer = SDL_CreateRenderer(*Window, -1, SDL_RENDERER_ACCELERATED);
+	if (*Renderer == NULL)
+	{
+		printf("Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
+		SDL_DestroyWindow(*Window);
+		SDL_Quit();
+		return (false);
+	}
+
+	return (true);
+}
+
+/**
+ * gameLoop - The main loop of the game, handling rendering and game logic.
+ * @Renderer: A pointer to the SDL_Renderer used for rendering textures.
+ * @wallTexture: A pointer to the SDL_Texture representing the wall texture.
+ * @floorTexture: A pointer to the SDL_Texture representing the floor texture.
+ * @mapFile: Path to the map file.
+ */
+
+void gameLoop(SDL_Renderer *Renderer, SDL_Texture *wallTexture,
+			  SDL_Texture *floorTexture, const char *mapFile)
+{
+	bool quit = false, showMap = false, moveForward = false, moveBackward = false;
+	bool moveRight = false, rotateLeft = false;
+	bool rotateRight = false, moveLeft = false;
+
 	while (!quit)
 	{
 		SDL_Event e;
@@ -147,11 +95,53 @@ int main(int argc, char *args[])
 		/* Render the scene */
 		SDL_SetRenderDrawColor(Renderer, 0x00, 0x00, 0x00, 0xFF);
 		SDL_RenderClear(Renderer);
-		parseMap(args[1], map);
+		parseMap(mapFile, map);
 		castRays(Renderer, wallTexture, floorTexture);
 		drawMapOnWindow(Renderer, &showMap);
 		SDL_RenderPresent(Renderer);
 	}
+}
+
+/**
+ * main - Entry point to the game.
+ * @argc: Command line arguments number.
+ * @args: Command line arguments.
+ * Return: 0 on success.
+ */
+
+int main(int argc, char *args[])
+{
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: %s <map file>\n", args[0]);
+		return (EXIT_FAILURE);
+	}
+
+	SDL_Window *Window = NULL;
+	SDL_Renderer *Renderer = NULL;
+	SDL_Texture *wallTexture = NULL, *floorTexture = NULL;
+
+	if (!initializeSDL(&Window, &Renderer))
+	{
+		return (EXIT_FAILURE);
+	}
+
+	if (!loadTextures(&wallTexture, &floorTexture, Renderer))
+	{
+		fprintf(stderr, "Failed to load textures\n");
+		destroySDL(wallTexture, floorTexture, Renderer, Window);
+		return (EXIT_FAILURE);
+	}
+
+	if (!parseMap(args[1], map))
+	{
+		fprintf(stderr, "Failed to parse map file: %s\n", args[1]);
+		destroySDL(wallTexture, floorTexture, Renderer, Window);
+		return (EXIT_FAILURE);
+	}
+
+	gameLoop(Renderer, wallTexture, floorTexture, args[1]);
+
 	destroySDL(wallTexture, floorTexture, Renderer, Window);
 	return (0);
 }
